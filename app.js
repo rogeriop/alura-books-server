45c59f8ed0178cb6d81f2c8dcfbd988309921ff9
rogeriop <pinheiroxrogerio@gmail.com>
2026-01-13 17:12:17 -0300

    02.Requisição com Axios [ instalaçaõ e congfiguração do cors ]

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const rotasLivro = require("./rotas/livro");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({origin: "*"})); 
 
 app.use("/livros", rotasLivro);
 
